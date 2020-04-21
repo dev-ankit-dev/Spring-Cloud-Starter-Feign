c507c79f667199e4cfe218e2a4b1de5716c36326
ankit <ashumca108@gmail.com>
2020-04-22 03:43:33 +0530

    delete some unnesssory code

--- src/main/java/com/test/Student/StudentProviderApplication.java
+++ src/main/java/com/test/Student/StudentProviderApplication.java
@@ -11,7 +11,7 @@ public class StudentProviderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentProviderApplication.class, args);
 		
-		System.out.println("https://github.com/dev-ankit-dev/Spring-Cloud-Starter-Feign.git");
+		
 		
 		
 	}
