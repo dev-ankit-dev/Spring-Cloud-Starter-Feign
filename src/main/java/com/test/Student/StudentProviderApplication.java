d7ffc129d8f0ff05fa0edf45a5adebe5dac11fd6
ankit <ashumca108@gmail.com>
2020-04-22 03:10:09 +0530

    code deleted from main

--- src/main/java/com/test/Student/StudentProviderApplication.java
+++ src/main/java/com/test/Student/StudentProviderApplication.java
@@ -11,7 +11,7 @@ public class StudentProviderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentProviderApplication.class, args);
 		
-		System.out.println("local");
+		
 		
 		
 	}
