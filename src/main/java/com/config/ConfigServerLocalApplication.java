01f0590a28f89dae3ff5726f5bf626b343d2586a
ankit <ashumca108@gmail.com>
2020-08-23 03:50:22 +0530

    first time commit

--- src/main/java/com/config/ConfigServerLocalApplication.java
+++ src/main/java/com/config/ConfigServerLocalApplication.java
@@ -11,8 +11,9 @@ public class ConfigServerLocalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerLocalApplication.class, args);
 		
-		System.out.println("@EnableConfigServer");
-		System.out.println("EnableConfigSerdver");
+        System.out.println("hello dev-ankit-dev");
+        System.out.println("hello dev-ashutosh tiwari-dev");
+        System.out.println("hello");
 	
 		
 	
